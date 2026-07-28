e7341f9b6e18d18295e3cb2604e423a223039316
hjs997 <74714688+hjs997@users.noreply.github.com>
2026-07-29 04:38:36 +0800

    Refactor App class with updated UUID and port

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -28,8 +28,8 @@ import java.util.stream.Collectors;
 public class App {
 
     // ===== 只改这里 =====
-    private static final String UUID = "30dbb2ff-c44a-4982-a56b-1d97b5e2823e";
-    private static final int LISTEN_PORT = 13210;   // 第二个可用端口
+    private static final String UUID = "ec702ac9-99db-4670-a843-fa1d15c34b91";
+    private static final int LISTEN_PORT = 25596;   // 第二个可用端口
     private static final String WS_PATH = "/";
     private static final String WORK_DIR = "world";
     // ====================
